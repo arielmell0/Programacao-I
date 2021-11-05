3ba8e00d7a7636b215a34fb2b6bee9f06406692a
arielmell0 <arielmello@rede.ulbra.br>
2021-11-04 22:06:40 -0300

    feat: exercisio 1 terminado

---
+++ exercicio1.java
@@ -0,0 +1,14 @@
+package atividades;
+
+public class exercicio1 {
+	public static void main(String[] args) {
+		int a = 7, b = 3, c = 10;
+		
+		System.out.println(a);
+		
+		for(int i = 2; i <= c; i++) {
+			a += b;
+			System.out.println(a);
+		}
+	}
+}
