80175e4c808f30691358786f2c07da71d5fca570
arielmell0 <arielmello@rede.ulbra.br>
2021-11-15 17:25:14 -0300

    feat: atividades terminadas

--- exercicio2.java
+++ exercicio2.java
@@ -3,8 +3,14 @@ package atividades;
 public class exercicio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			System.out.println(5/0);
+		} catch(Throwable erro) {
+			System.out.print("Excessão: ");
+			System.out.println(erro);
+		} finally {
+			System.out.println("Ops! você não pode dividir um número por 0!");
+		}
 	}
-
+w
 }
